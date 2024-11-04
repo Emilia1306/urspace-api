f1e0cbf1c38c3d37aded33662018e5c64e0013bb
Emilia Escobar <emiliaescobar105@gmail.com>
2024-11-04 17:08:35 -0600

    Reservas

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
 import authRoutes from './routes/authRoutes'; // Importa authRoutes correctamente
 import terrenoRoutes from './routes/terrenoRoutes';
+import reservaRoutes from './routes/reservaRoutes';
 import path from 'path';
 
 const app = express();
@@ -17,5 +18,7 @@ app.use('/api/auth', authRoutes); // Rutas de autenticación
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 app.use('/api', terrenoRoutes);
+
+app.use('/api', reservaRoutes); // Añade las rutas de reservas
 
 app.listen(3000, () => console.log('Server running on port 3000'));
