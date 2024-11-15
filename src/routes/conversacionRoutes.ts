45c2bfe5c2a65c4aa35dab66261821f484c7caff
Emilia Escobar <emiliaescobar105@gmail.com>
2024-11-15 02:43:05 -0600

    Modificacion de conversaciones

--- src/routes/conversacionRoutes.ts
+++ src/routes/conversacionRoutes.ts
@@ -7,6 +7,18 @@ const router = express.Router();
 router.post("/conversaciones", ConversacionController.create);
 
 // Obtener conversaciones de un usuario
-router.get("/conversaciones/usuario/:id_usuario", ConversacionController.getConversacionesByUsuario);
+router.get(
+  "/conversaciones/usuario/:id_usuario",
+  ConversacionController.getConversacionesByUsuario
+);
+
+//Encontrar o crear conversación
+router.post(
+  "/conversaciones/find-or-create",
+  ConversacionController.findOrCreateConversation
+);
+
+// Ruta para obtener una conversación por su ID
+router.get("/conversaciones/:id_conversacion", ConversacionController.getConversacionById);
 
 export default router;
